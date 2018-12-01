a07216606d4db6086c59ee81e88d92e59e9eae5e
Dante <andi.etnad@gmail.com>
2018-12-01 21:18:10 +0000

    improved simplicity in code

--- create_db_usr.sh
+++ create_db_usr.sh
@@ -5,57 +5,51 @@ PASSWD="$(openssl rand -base64 12)"
 
 # If /root/.my.cnf exists then it won't ask for root passwd
 if [ -f /root/.my.cnf ]; then
-	echo "Please enter the NAME of the new database!"
-	read dbname
-	echo "Please enter the database CHARACTER SET! (ex: latin1, utf8, ...)"
-	read charset
+	read -p "Please enter the NAME of the new database: " DBNAME
+	read -p "Please enter the database CHARACTER SET! (ex: latin1, utf8, ...): " CHARSET
 	echo "Creating new database..."
-	mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	mysql -e "CREATE DATABASE ${DBNAME} /*\!40100 DEFAULT CHARACTER SET ${CHARSET} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
 	mysql -e "show databases;"
 	echo ""
-	echo "Please enter the NAME of the new database user!"
-	read username
+	read -p "Please enter the NAME of the new database user: " USERNAME
 	echo "Creating new user..."
-	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${PASSWD}';"
+	mysql -e "CREATE USER ${USERNAME}@localhost IDENTIFIED BY '${PASSWD}';"
 	echo "User successfully created!"
-    	echo "+ User: $username"
+    echo "+ User: $USERNAME"
+    echo "+ DBName: $DBNAME"
    	echo "+ Passwd: $PASSWD"
 	echo ""
-	echo "Granting ALL privileges on ${dbname} to ${username}!"
-	mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	echo "Granting ALL privileges on ${DBNAME} to ${USERNAME}!"
+	mysql -e "GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${USERNAME}'@'localhost';"
 	mysql -e "FLUSH PRIVILEGES;"
 	echo "We are good to go!"
 	exit
 	
 # If /root/.my.cnf doesn't exist then it'll ask for root password	
 else
-	echo "Please enter root user MySQL password!"
-	read rootpasswd
-	echo "Please enter the NAME of the new database!"
-	read dbname
-	echo "Please enter the database CHARACTER SET! (example: latin1, utf8, ...)"
-	read charset
+	read -p "Please enter root MySQL password: " ROOTPASSWD
+	read -p "Please enter the NAME of the new database: " DBNAME
+	read -p "Please enter the database CHARACTER SET! (example: latin1, utf8, ...): " CHARSET
 	echo "Creating new database..."
-	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
+	mysql -uroot -p${ROOTPASSWD} -e "CREATE DATABASE ${DBNAME} /*\!40100 DEFAULT CHARACTER SET ${CHARSET} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
-	mysql -uroot -p${rootpasswd} -e "show databases;"
+	mysql -uroot -p${ROOTPASSWD} -e "show databases;"
 	echo ""
-	echo "Please enter the NAME of the new database user!"
-	read username
-	echo "Please enter the PASSWORD for the new database user!"
-	read userpass
+	read -p "Please enter the NAME of the new database user: " USERNAME
+	read -p "Please enter the PASSWORD for the new database user: " USERPASS
 	echo "Creating new user..."
-	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	mysql -uroot -p${ROOTPASSWD} -e "CREATE USER ${USERNAME}@localhost IDENTIFIED BY '${USERNAME}';"
 	echo "User successfully created!"
-    	echo "+ User: $username"
+    echo "+ User: $USERNAME"
+    echo "+ DBName: $DBNAME"
    	echo "+ Passwd: $PASSWD"
 	echo ""
-	echo "Granting ALL privileges on ${dbname} to ${username}!"
-	mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
-	mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+	echo "Granting ALL privileges on ${DBNAME} to ${USERNAME}!"
+	mysql -uroot -p${ROOTPASSWD} -e "GRANT ALL PRIVILEGES ON ${DBNAME}.* TO '${USERNAME}'@'localhost';"
+	mysql -uroot -p${ROOTPASSWD} -e "FLUSH PRIVILEGES;"
 	echo "You're good now :)"
 	exit
 fi
