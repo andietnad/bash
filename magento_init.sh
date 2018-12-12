c54e18d8b11200b6b854fcd0f9058ed67610edde
Andi <a.apetroaei@e-tailors.com>
2018-12-12 18:29:02 +0000

    version 2

--- magento_init.sh
+++ magento_init.sh
@@ -45,4 +45,4 @@ bin/magento setup:install \
 
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
-php bin/magento setup:static-content:deploy
+php bin/magento setup:static-content:deploy -f
